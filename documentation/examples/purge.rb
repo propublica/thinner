1c4ad42ad7a6cee60f2314aa5534491f466b505a
Jeff Larson <thejefflarson@gmail.com>
2010-11-02 14:51:05 -0400

    showcasing .* usage

--- documentation/examples/purge.rb
+++ documentation/examples/purge.rb
@@ -1,5 +1,5 @@
 # The urls in this array are purged in order, so you'll want to structure it
 # according to usage.
-arr << ["/some_route", "/"]
+arr << ["/some_route", "/some_route/with.*", "/"]
 
 Thinner.purge! arr
